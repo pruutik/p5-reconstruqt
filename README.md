2a0e2603df479248149e0ffc1890e3911d83e6fb
pruutik <x-shimmer-x@live.com>
2023-05-14 12:14:35 +0600

    prepare repo for github

---
+++ README.md
@@ -0,0 +1,4 @@
+# p5-reconstruqt
+A reproduction of the [reconstruqt album page's](http://reconstruqt.fusz.jp/) background using p5.js and typescript.
+
+**Note: The sketch.js file has been manually edited after compilation to make it compatible with browsers.
---
+++ main.ts
@@ -0,0 +1,85 @@
+import P5 from 'p5';
+
+type SketchVars = {
+	distance : number;
+	screenSize : number;
+	numberOfNodes : number;
+	maxSpeed : number;
+	nodes : Node[];
+}
+
+const sketch = (p5: P5 & SketchVars) => {
+	
+	p5.distance = 70;
+	p5.screenSize = 400;
+	p5.numberOfNodes = 50;
+	p5.maxSpeed = 2;
+
+	p5.nodes = [];
+
+	// The sketch setup method 
+	p5.setup = () => {
+		// Creating and positioning the canvas
+		const canvas = p5.createCanvas(p5.windowWidth, p5.windowHeight);
+
+		// Configuring the canvas
+		p5.background("black");
+		for (let i = 0; i < p5.numberOfNodes; i ++) {
+			p5.nodes.push(new Node(p5.random(p5.windowWidth), p5.random(p5.windowHeight), p5.random(p5.maxSpeed), p5.random(-1,1), p5.random(-1,1)));
+		}
+	};
+
+	// The sketch draw method
+	p5.draw = () => {
+		p5.background("black");
+		for (let i = 0; i < p5.numberOfNodes; i ++) {
+			p5.nodes[i].move(p5);
+			p5.nodes[i].display(p5);
+		}
+	};
+};
+
+var p5sketch = new P5(sketch);
+ 
+class Node {
+	xpos : number;
+	ypos : number;
+	speed : number;
+	rise : number;
+	run : number;
+   
+  	constructor(xpos_ : number, ypos_ : number, speed_ : number, rise_ : number, run_ : number) {
+		this.xpos = xpos_;
+		this.ypos = ypos_;
+		this.speed = speed_;
+		this.run = run_;
+		this.rise = rise_;
+  	}
+   
+  	display(p5 : P5 & SketchVars) {
+		p5.noStroke();
+		p5.fill(0,0,0,30);
+		p5.circle(this.xpos, this.ypos, 1);
+		for (let i = 0; i < p5.nodes.length; i ++) {
+			if(p5.nodes[i]===this)continue;
+			let strokeWeight = p5.sqrt(p5.abs(this.xpos - p5.nodes[i].xpos) ** 2 + p5.abs(this.ypos - p5.nodes[i].ypos) ** 2);
+			p5.stroke(255, 40+p5.distance - strokeWeight);
+			p5.line(this.xpos, this.ypos, p5.nodes[i].xpos, p5.nodes[i].ypos);
+		}
+  	}
+   
+  	move(p5 : P5 & SketchVars) {
+		this.xpos = this.xpos + (this.speed * this.run);
+		this.ypos = this.ypos + (this.speed * this.rise);
+		if (this.xpos > p5.windowWidth) {
+			this.xpos = 0;
+		} else if (this.xpos < 0) {
+			this.xpos = p5.windowWidth;
+		}
+		if (this.ypos > p5.windowHeight) {
+			this.ypos = 0;
+		} else if (this.ypos < 0) {
+			this.ypos = p5.windowHeight;
+		}  
+  	}
+}
